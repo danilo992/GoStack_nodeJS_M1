b3f3b822916d7c860dace4c8b2339cbe6531f494
You Name <tadeudanilo46@gmail.com>
2020-01-28 10:13:56 -0300

    usando os params Query, Route e Request body

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 
 const app = express();
+
+//query params = ?test=1
+//Route params = /users/1
+//Request body = {"name": "Tadeu", "email": "tadeudanilo46@gmail.com"}
  
-app.get('/test', (req, res) => {
-  return res.json( { messege: "hello word" } );
+app.get('/users/:id', (req, res) => {
+
+  const {id} = req.params;
+
+  return res.json({menssege: `buscndo... ${id}`}) ;
 });
 
 app.listen(3000)
