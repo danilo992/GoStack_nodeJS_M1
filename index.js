6ad56c6252791dc042633414b78c4779f0c3d999
You Name <tadeudanilo46@gmail.com>
2020-01-29 21:55:51 -0300

    CRUD create, Read, Update e Delete. Usando o http get, post, push e delete

--- index.js
+++ index.js
@@ -2,16 +2,40 @@ const express = require('express');
 
 const app = express();
 
+app.use(express.json());
+
 //query params = ?test=1
 //Route params = /users/1
 //Request body = {"name": "Tadeu", "email": "tadeudanilo46@gmail.com"}
-const users = ["Danilo", " Tadeu", "da", "silva"]
+
+// CRUD - Create, Read, Update, Delete
+const users = ['danilo', 'tadeu', 'Lindi', 'Lorena'];
+
+app.get('/users', (req, res) => {
+  return res.json(users);
+})
 
 app.get('/users/:index', (req, res) => {
+  const { index } = req.params;
 
-  const {index} = req.params;
-
-  return res.json(users[index]) ;
+  return res.json(users[index]);
 });
+
+app.get('/teste', (req, res) => {
+
+  const nome = req.query.name;
+
+  return res.json({messege: `${nome} sucess...` })
+});
+
+app.post('/users', (req, res) => {
+  const { name } = req.body;
+
+  users.push(name);
+
+  return res.json(users)
+})
+
+
 
 app.listen(3000);
